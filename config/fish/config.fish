8e767f084d7c79e707ab1738370b8b522d0af327
Chrysanthos Panagakos <chpanagakos@gmail.com>
2025-11-17 23:35:42 +0200

    Restructured dotfiles with stow, added fish/starship/redshift

---
+++ config/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Disable the default greeting
+    set -g fish_greeting
+
+    # Initialize Starship
+    starship init fish | source
+
+    # --- Fish Abbreviations (The better alias) ---
+    
+    # Format: abbr --add [SHORTCUT] [COMMAND]
+    
+    abbr --add ll ls -l
+    abbr --add la ls -la
+    abbr --add gs git status
+    abbr --add ga git add
+    abbr --add gc git commit -m
+    
+    # Use 'alias' only if you really need it to NOT expand
+    # alias my_alias 'echo hello'
+end
