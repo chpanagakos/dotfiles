b99de5bcc37382ed4325199c7abef7963bec230c
Chrysanthos Panagakos <chpanagakos@gmail.com>
2026-06-20 13:38:13 +0300

    change priority to brave

--- bin/webapp-menu.sh
+++ bin/webapp-menu.sh
@@ -27,12 +27,12 @@ SELECTED=$(echo "$SITES" | dmenu -i -p "Web App:" -l 7)
 URL=$(echo "$SELECTED" | cut -d'|' -f2 | xargs)
 
 # Browser Selection Logic
-if command -v chromium >/dev/null 2>&1; then
+if command -v brave-browser >/dev/null 2>&1; then
+    BROWSER="brave-browser"
+elif command -v chromium >/dev/null 2>&1; then
     BROWSER="chromium"
 elif command -v google-chrome-stable >/dev/null 2>&1; then
     BROWSER="google-chrome-stable"
-elif command -v brave-browser >/dev/null 2>&1; then
-    BROWSER="brave-browser"
 else
     firefox --new-window "$URL"
     exit 0
